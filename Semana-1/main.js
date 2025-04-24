37c423fa873217acf2ce4a680cf8e54d7ff50df5
NachoBasilio <ignacio.n.basilio.b@gmail.com>
2025-04-24 20:52:12 -0300

    Fin primera clase

---
+++ Semana-1/main.js
@@ -0,0 +1,42 @@
+console.log("Hola, mundo!")
+// console.dir()
+// console.error()
+
+let edad = 26
+
+console.log(edad)
+
+console.log(typeof edad)
+
+edad = "Nacho"
+
+console.log("Edad: ", edad)
+
+console.log(typeof edad)
+
+edad = false
+
+console.log("Edad: " + edad)
+
+const PI = 3.14
+
+const URL = "https://www.google.com"
+
+
+console.log(PI)
+
+console.log(URL) // "'Una variable que se autopercibe no varible' - Cristian Cilio"
+
+console.log(typeof edad)
+
+//alert("Hola")
+
+//let confirmacion = confirm("Esta seguro de que quiere facturar esto")
+
+//console.log(confirmacion)
+
+//let clubDelUsuario = prompt("De que club sos?")
+
+//console.log(clubDelUsuario)
+
+console.log( 2 / "2" )
